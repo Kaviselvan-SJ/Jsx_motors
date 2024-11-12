65a65fda3d9aa4a40a7f7fafa06f0a5d4e5a1c20
kaviselvan <sjkaviselvan07@gmail.com>
2024-11-13 00:29:42 +0530

    Initial Commit

--- bookstore/src/main/java/AdminMenu.java
+++ bookstore/src/main/java/AdminMenu.java
@@ -15,7 +15,7 @@ public class AdminMenu extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.getWriter().append("Served at:").append(request.getContextPath());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
